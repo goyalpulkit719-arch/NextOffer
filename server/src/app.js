b676d88ee7bc62a09de8658ed8f80333dad027a8
pulkit-first <goyalpulkit719@gmail.com>
2026-07-20 12:39:42 +0530

    Update CORS for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,10 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: [
+        "http://localhost:5173",
+        process.env.CLIENT_URL,
+    ],
     credentials: true,
 }));
 
